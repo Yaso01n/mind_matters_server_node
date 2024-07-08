82591012387ae386b82230c2c5861eee2a4f2075
Yasmin_Yasser <yasminyasser508@gmail.com>
2024-07-08 22:05:43 +0300

    undo

--- index.js
+++ index.js
@@ -12,83 +12,34 @@ const OrganizationRoute = require('./routes/organizationRouter');
 // const AppointmentRoute = require('./routes/appointmentRouter');
 // const RecordRoute = require('./routes/recordRouter');
 
+
 const MindMatters_app = express();
+MindMatters_app.use(cors());
+const PORT =  process.env.PORT || 3000;
 
-// Set CORS options to allow requests from any origin
-MindMatters_app.use(cors({ origin: '*' }));
-
-const PORT = process.env.PORT || 3000;
+// //====================================================================
+// MindMatters_app.use(cors({                     
+//   origin: 'http://localhost:3000',
+// }));
 
 connectionModule.connect((err) => {
-  if (err) {
+  if (err){
     console.error('Error connecting to database:', err);
     return;
   }
 
   console.log('DATABASE CONNECTED');
-
+  
   MindMatters_app.listen(PORT, () => {
-    console.log(`SERVER: https://mindmattersservernode-git-main-yaso01ns-projects.vercel.app:${PORT}`);
+    console.log(`SERVER: https://mindmattersservernode-git-main-yaso01ns-projects.vercel.app/${PORT}`);
   });
 });
-
+//====================================================================
 MindMatters_app.use(express.json());
 MindMatters_app.use('/', UserRoute);
 // MindMatters_app.use('/', DoctorRoute);
 MindMatters_app.use('/', OrganizationRoute);
 // MindMatters_app.use('/', AppointmentRoute);
 // MindMatters_app.use('/', RecordRoute);
-
-// Catch-all handler for all other requests
-MindMatters_app.use((req, res, next) => {
-  res.status(404).send('404: NOT_FOUND');
-});
-
-
-
-
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-
-// dotenv.config(); // Load environment variables from .env file
-
-// const connectionModule = require('./DataBase/connection');
-
-// const UserRoute = require('./routes/userRouter');
-// // const DoctorRoute = require('./routes/doctorRouter');
-// const OrganizationRoute = require('./routes/organizationRouter');
-// // const AppointmentRoute = require('./routes/appointmentRouter');
-// // const RecordRoute = require('./routes/recordRouter');
-
-
-// const MindMatters_app = express();
-// MindMatters_app.use(cors());
-// const PORT =  process.env.PORT || 3000;
-
-// // //====================================================================
-// // MindMatters_app.use(cors({                     
-// //   origin: 'http://localhost:3000',
-// // }));
-
-// connectionModule.connect((err) => {
-//   if (err){
-//     console.error('Error connecting to database:', err);
-//     return;
-//   }
-
-//   console.log('DATABASE CONNECTED');
-  
-//   MindMatters_app.listen(PORT, () => {
-//     console.log(`SERVER: https://mindmattersservernode-git-main-yaso01ns-projects.vercel.app/${PORT}`);
-//   });
-// });
-// //====================================================================
-// MindMatters_app.use(express.json());
-// MindMatters_app.use('/', UserRoute);
-// // MindMatters_app.use('/', DoctorRoute);
-// MindMatters_app.use('/', OrganizationRoute);
-// // MindMatters_app.use('/', AppointmentRoute);
-// // MindMatters_app.use('/', RecordRoute);
 
 
